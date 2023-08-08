0a5cfd7798e9a299045d36b798719a186afb2752
Keithjmm <joenormal79@gmail.com>
2023-08-08 21:56:55 +0000

    add models.py code

--- gastro_app/models.py
+++ gastro_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Reservations(models.Model):
+    """Reservations model definitions"""
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=20)
+    email = models.EmailField(max_length=100)
+    date = models.DateField()
+    time = models.TimeField()
+    guests = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.name
