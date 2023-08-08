e7c0545968ac391289e92cfa3569b466318a70d7
Keithjmm <joenormal79@gmail.com>
2023-08-08 23:22:17 +0000

    updated database settings

--- p_gastro/settings.py
+++ p_gastro/settings.py
@@ -81,10 +81,7 @@ WSGI_APPLICATION = 'p_gastro.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    }
+}
 
 
 
